6f27ae039a087bd15087320297b402bf2bf8d6a4
EugenePoloshovets <jeksoncheggg@gmail.com>
2022-01-27 19:36:01 +0300

    Initial commit

---
+++ src/test/java/com/payau/blockwitlearn/BlockwitLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payau.blockwitlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockwitLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
