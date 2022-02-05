6450e244c7c69a22ef54e9d6e706694f0fa914db
EugenePoloshovets <jeksoncheggg@gmail.com>
2022-02-05 13:43:07 +0300

    add thymeleaf, add h1 home-page

--- src/main/java/com/payau/blockwitlearn/BlockwitLearnApplication.java
+++ src/main/java/com/payau/blockwitlearn/BlockwitLearnApplication.java
@@ -14,8 +14,4 @@ public class BlockwitLearnApplication {
         SpringApplication.run(BlockwitLearnApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
-    }
 }
